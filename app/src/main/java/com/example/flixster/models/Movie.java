af703dceb57be0ae6d6a037f07d789bad0153bf6
tykm <tylerkim01@gmail.com>
2022-02-08 20:40:40 -0500

    Movie object is now Parcelable

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,16 +3,21 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel // annotation indicates class is Parcelable
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+
+    // Must have empty constructor for Parcel
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
