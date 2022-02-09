053ced80042272fe108e9a2abb76266b4a0fe53d
tykm <tylerkim01@gmail.com>
2022-02-08 21:46:56 -0500

    Detail view complete with Title, Rating, Overview

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.flixster.models.Movie;
 
@@ -14,12 +16,28 @@ public class MovieDetailsActivity extends AppCompatActivity {
     // The movie we are displaying
     Movie movie;
 
+    // The view objects
+    TextView tvDetailTitle;
+    RatingBar rbVoteAverage;
+    TextView tvDetailOverview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // Resolve the view objects
+        tvDetailTitle = (TextView) findViewById(R.id.tvDetailTitle);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        tvDetailOverview = (TextView) findViewById(R.id.tvDetailOverview);
+
         // Unwrap the parceled movie intent using the SimpleName as key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // Set the title and overview
+        tvDetailTitle.setText(movie.getTitle());
+        tvDetailOverview.setText(movie.getOverview());
+        rbVoteAverage.setRating(movie.getVoteAverage().floatValue() / 2.0f);
     }
 }
